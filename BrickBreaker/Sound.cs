9b4645d74afa7c6c47fd411a8935ceb0a471c550
Noi Barkayev <Noi6023@gmail.com>
2020-12-22 08:14:45 +0200

    Audio enum included

---
+++ BrickBreaker/Sound.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrickBreaker
+{
+    public enum Sound
+    {
+        //public static string[] sounds = { "brick_hit", "bat_hit", "music", "finished_bricks", "lost" };
+        brick_hit,
+        bat_hit,
+        music,
+        finished_bricks,
+        lost
+    }
+}
